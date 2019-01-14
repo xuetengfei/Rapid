f8293d1323671f39873d34b315a810edaf886c61
xuetengfei <435408212@qq.com>
2019-01-14 16:42:36 +0800

    first test

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
-#!/usr/bin/env node   //告诉node使用终端运行
-const fs = require("fs"); //文件系统
+#!/usr/bin/env node
+const fs = require("fs");
 const program = require("commander"); //终端输入处理框架
-const packageJson = require("./package.json"); //获取版本信息
-
+const package = require("./package.json"); //获取版本信息
 program
-  .version(packageJson.version, "-v,--version","show your version")
-  .description("xuetengfei node command line")
-  .option("-p, --peppers", "Add peppers")
-  .option("-P, --pineapple", "Add pineapple")
-  .option("-b, --bbq", "Add bbq sauce")
-  .option(
-    "-c, --cheese [type]",
-    "Add the specified type of cheese [marble]",
-    "marble"
-  )
-  .parse(process.argv);
-
-console.log("hi,you ordered a pizza with:");
-if (program.peppers) console.log("  - peppers");
-if (program.pineapple) console.log("  - pineapple");
-if (program.bbq) console.log("  - bbq");
-console.log("  - %s cheese", program.cheese);
-
-program
-	.command("setup")
-	.description("setup command is running...")
-  .action(function(){
-	 console.log('log setup')
-	})
-
+  .version(package.version, "-v,--version")
+  .command("init <name>")
+  .action(name => {
+    console.log(name);
+  });
 program.parse(process.argv);
